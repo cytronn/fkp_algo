6aed05a413c6b4967a6eaa23cd42f7cb9a378b3f
Arnaud Villani <arnaud.villani@gmail.com>
2016-02-08 13:28:31 +0100

    added Prisons model

---
+++ models/Prisons.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var PrisonSchema = new mongoose.Schema({
+  coordinates: {x: Number, y: Number},
+  interregional_direction: { type: mongoose.Schema.Types.ObjectId, ref: 'DI' },
+  population: Number,
+  density: Number,
+  family: String,
+  family_id: Number,
+});
+
+
+
+mongoose.model('Prison', CommentSchema);
+
+
