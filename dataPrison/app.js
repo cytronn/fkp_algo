5e75eb1ef110906d2ee8387d25b23e56e65b7d50
Arnaud Villani <arnaud.villani@gmail.com>
2016-02-09 14:51:59 +0100

    Added the "/" route
    sur la route de Memphis

--- dataPrison/app.js
+++ dataPrison/app.js
@@ -66,5 +66,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-c to terminate');
+});
+
+
+module.exports = app;
 
 module.exports = app;
--- dataPrison/routes/index.js
+++ dataPrison/routes/index.js
@@ -1,9 +1,19 @@
+var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
+var Prison = mongoose.model('Prison');
+var DI = mongoose.model('DI');
+var Family = mongoose.model('Family');
+var Fact = mongoose.model('Fact');
+var Resource = mongoose.model('Resource');
+var Country = mongoose.model('Country');
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('back', { title: 'Express' });
+  res.render('back', console.log('hello world'));
 });
 
 module.exports = router;
