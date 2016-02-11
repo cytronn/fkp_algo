b406f053d39a7b30d4684125b1aea21a68f53aeb
clarzou <clairezunda@gmail.com>
2016-02-11 11:18:59 +0100

    phoque chill

--- dataPrison/app.js
+++ dataPrison/app.js
@@ -73,5 +73,3 @@ app.listen(app.get('port'), function() {
 
 
 module.exports = app;
-
-module.exports = app;
--- dataPrison/routes/index.js
+++ dataPrison/routes/index.js
@@ -8,12 +8,29 @@ var Fact = mongoose.model('Fact');
 var Resource = mongoose.model('Resource');
 var Country = mongoose.model('Country');
 
-
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('back', console.log('hello world'));
+});
+
+/* POST TO RESOURCES*/
+
+router.get('/resources', function(req, res, next) {
+  Resource.find(function(err, resources){
+    if(err){ return next(err); }
+
+    res.json(resources);
+  });
+});
+
+router.post('/resources', function(req, res, next) {
+  var resource = new Resource(req.body);
+
+  resource.save(function(err, resource){
+    if(err){ return next(err); }
+
+    res.json(resource);
+  });
 });
 
 module.exports = router;
