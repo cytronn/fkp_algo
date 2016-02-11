ec1d68247db03b0142df1ecc6a230359f6b64309
Arnaud Villani <arnaud.villani@gmail.com>
2016-02-11 17:52:41 +0100

    post a ressource to db
    Added the resources factory
    Added create method

--- dataPrison/public/javascripts/angularApp.js
+++ dataPrison/public/javascripts/angularApp.js
@@ -18,7 +18,29 @@ app.config([
 }]);
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
+'$scope', 'resources',
+function($scope, resources){
   $scope.test = 'Hello world!';
+
+  $scope.addResource = function() {
+    resources.create({
+      name: $scope.name, 
+      link: $scope.link,
+    });
+  };
+}]);
+
+
+app.factory('resources', ['$http', function($http) {
+  var o = {
+    resources: [],
+  };
+
+  o.create = function(resource) {
+    return $http.post('/resources', resource)
+      .success(function(data) {
+        o.resources.push(data);
+      });
+  };
+  return o;
 }]);
