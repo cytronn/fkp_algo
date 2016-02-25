c60cde701842cbf7a6a2719c09dd86b26189fcec
Lionel Paulus <hello@lionelpaul.us>
2016-02-25 12:56:29 +0100

    New port for server

--- dataPrison/app.js
+++ dataPrison/app.js
@@ -20,6 +20,9 @@ var routes = require('./routes/index');
 
 var app = express();
 
+// Change port
+app.set('port', process.env.PORT || 8080);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
