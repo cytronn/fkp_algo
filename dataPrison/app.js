097681035d6731e73b4c6cec065b5b2c916effc4
Lionel Paulus <hello@lionelpaul.us>
2016-02-25 12:59:01 +0100

    New port

--- dataPrison/app.js
+++ dataPrison/app.js
@@ -21,7 +21,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // Change port
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 4000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
