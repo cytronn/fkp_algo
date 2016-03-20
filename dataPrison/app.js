e90297ca8e2cc38c43d788c4ad038fed0bf7a179
Lionel Paulus <hello@lionelpaul.us>
2016-03-20 15:32:24 +0100

    Revert "Commenting res.header allow-origin"
    This reverts commit a5490d0c7cfbdac4c7f679b3434576901f5269b0.

--- dataPrison/app.js
+++ dataPrison/app.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type,Authorization");
   res.header("Access-Control-Allow-Credentials", true);
